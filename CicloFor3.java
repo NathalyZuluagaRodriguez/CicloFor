238d727e5d2a22e1fde2b9ff9781b88650d32d00
unknown <nathrodriguez222@gmail.com>
2024-07-27 20:37:29 -0500

    Ejercicios for

---
+++ CicloFor3.java
@@ -0,0 +1,12 @@
+
+public class CicloFor3 {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            suma += i;
+        }
+
+        System.out.println("La suma de los primeros 5 números naturales es: " + suma);
+    }
+ }
